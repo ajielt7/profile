294f23897263acda12342a20e2612311960cf3f3
ajielt7 <ajielt7@gmail.com>
2020-06-28 12:09:48 +0700

    menambah fitur paralax

--- script.js
+++ script.js
@@ -10,3 +10,25 @@
 //   // );
 //   event.preventDefault();
 // });
+$(window).scroll(function () {
+  var wScroll = $(this).scrollTop();
+  if (wScroll > $(".service").offset().top - 450) {
+    $(".service .card").each(function (i) {
+      $(".pKiri").addClass("pMuncul");
+      $(".pKanan ").addClass("pMuncul");
+    });
+  }
+});
+
+$(window).on("load", function () {
+  $(".jumbotron").addClass("pMuncul");
+});
+
+$(window).scroll(function () {
+  var wScroll = $(this).scrollTop();
+  if (wScroll > $(".service").offset().top - 100) {
+    $(".service .card").each(function (i) {
+      $(".footer").addClass("pMuncul");
+    });
+  }
+});
