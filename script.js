aa4d58704035047b011d9209f91863acb5e0c412
ajielt7 <ajielt7@gmail.com>
2020-06-28 11:13:57 +0700

    menambah file js dan midifikasi index.html dan scrip.js

--- script.js
+++ script.js
@@ -1,33 +1,12 @@
-// paralax
+// $(".page-scroll").on("click", function (event) {
+//   var tujuan = $(this).attr("href");
+//   var elemenTujuan = $(tujuan);
 
-$(window).scroll(function() {
-    var wScroll = $(this).scrollTop();
-
-    $('.jumbotron img').css({
-        'transform' : 'translate(0px, '+ wScroll/5 +'%)'
-    });
-
-    $('.jumbotron h1').css({
-        'transform' : 'translate(0px, '+ wScroll/2.5 +'%)'
-    });
-
-    $('.jumbotron p').css({
-        'transform' : 'translate(0px, '+ wScroll/1 +'%)'
-    });
-
-    if ( wScroll > $('.portfolio').offset().top -250) {
-        $('.portfolio .card').each(function(i) {
-            setTimeout(function() {
-                $('.portfolio .card').eq(i).addClass('muncul');
-            }, 300 * (i+1));
-        })
-    }
-});
-
-
-// load about
-
-$(window).on('load', function(){
-    $('.pKiri').addClass('pMuncul');
-    $('.pKanan ').addClass('pMuncul');
-});
+//   console.log($("body").scrollTop());
+//   // $("body").animate(
+//   //   { scrollTop: elemenTujuan.offset().top - 50 },
+//   //   1250,
+//   //   "swing"
+//   // );
+//   event.preventDefault();
+// });
